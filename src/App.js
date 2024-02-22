f004bde954c9db85c5912214210c9419e5cf1e77
kplnhsyn <kplnhsyn.49@gmail.com>
2024-02-22 22:50:20 +0100

    feat:themeContext.js added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { ThemeProvider } from "styled-components";
 import AppRouter from "./router/AppRouter";
-import { lightTheme } from "./styles/theme";
+import { darktheme, lightTheme } from "./styles/theme";
 import { GlobalStyles } from "./styles/Global.styles";
+import { useThemeContext } from "./context/ThemeContext";
 
 function App() {
+    const { myTheme } = useThemeContext()
+    const themes = myTheme==="light"?lightTheme:darktheme
     return (
-        <div className="App">
-        <ThemeProvider theme={lightTheme}>
-          <GlobalStyles/>
-                <AppRouter />
-            </ThemeProvider>
-        </div>
+        <ThemeProvider theme={themes}>
+            <GlobalStyles />
+            <AppRouter />
+        </ThemeProvider>
     );
 }
 
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,1 +1,24 @@
 // Theme Context
+
+import { createContext, useContext, useState } from "react";
+
+const ThemeContext = createContext();
+
+const ThemeContextProvider = ({ children }) => {
+  const [myTheme, setMyTheme] = useState("light");
+    // const values = { myTheme, setMyTheme }
+    //  <ThemeContext.Provider value={values}>
+    //   {children}
+    // </ThemeContext.Provider>
+  return (
+    <ThemeContext.Provider value={{ myTheme, setMyTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useThemeContext = () => {
+    return useContext(ThemeContext)
+}
+
+export default ThemeContextProvider;
