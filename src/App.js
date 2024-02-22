23731349bdb9a5fe06d541fc8c619cdb1c316679
kplnhsyn <kplnhsyn.49@gmail.com>
2024-02-22 22:26:09 +0100

    feat:GlobalStyle created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import AppRouter from './router/AppRouter';
+import { ThemeProvider } from "styled-components";
+import AppRouter from "./router/AppRouter";
+import { lightTheme } from "./styles/theme";
+import { GlobalStyles } from "./styles/Global.styles";
 
 function App() {
-  return (
-    <div className="App">
-      <AppRouter/>
-    </div>
-  );
+    return (
+        <div className="App">
+        <ThemeProvider theme={lightTheme}>
+          <GlobalStyles/>
+                <AppRouter />
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
