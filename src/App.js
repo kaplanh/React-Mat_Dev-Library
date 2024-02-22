b4e324a30a3c792caa927ac2b58f806023075d31
kplnhsyn <kplnhsyn.49@gmail.com>
2024-02-22 23:41:12 +0100

    feat:PrivateRouter created

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import AppRouter from "./router/AppRouter";
 import { darktheme, lightTheme } from "./styles/theme";
 import { GlobalStyles } from "./styles/Global.styles";
 import { useThemeContext } from "./context/ThemeContext";
+import AuthContextProvider from "./context/AuthContext";
 
 function App() {
-    const { myTheme } = useThemeContext()
-    const themes = myTheme==="light"?lightTheme:darktheme
+    const { myTheme } = useThemeContext();
+    const themes = myTheme === "light" ? lightTheme : darktheme;
     return (
         <ThemeProvider theme={themes}>
             <GlobalStyles />
-            <AppRouter />
+            <AuthContextProvider>
+                <AppRouter />
+            </AuthContextProvider>
         </ThemeProvider>
     );
 }
