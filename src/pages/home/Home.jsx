f08b912fa2a354647fd17d383395e3678eb4b65f
kplnhsyn <kplnhsyn.49@gmail.com>
2024-02-23 03:53:07 +0100

    feat:Home.jsx added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import Header from "../../components/header/Header";
-
+import Card from "../../components/card/Card";
 import { CardContainer, HomeContainer, HomeImage } from "./Home.style";
 import homeImg from "../../assets/books.jpg";
-
+import { useBooksContext } from "../../context/BooksContext";
 
 const Home = () => {
+    const { myData } = useBooksContext();
     return (
         <HomeContainer>
             <Header />
-
-            <CardContainer wrap="wrap"></CardContainer>
-
-            <HomeImage>
-                <img src={homeImg} alt="Home" />
-            </HomeImage>
+            {myData.length ? (
+                <CardContainer wrap="wrap">
+                    {myData.map((item) => (
+                        <Card key={item.id} item={item} />
+                    ))}
+                </CardContainer>
+            ) : (
+                <HomeImage>
+                    <img src={homeImg} alt="Home" />
+                </HomeImage>
+            )}
         </HomeContainer>
     );
 };
